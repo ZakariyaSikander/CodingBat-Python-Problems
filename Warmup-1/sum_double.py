5fa19897c938f8c8f9138e3acbc0d81aebf7f5d9
ZakariyaSikander <zakariya.sikander@gmail.com>
2025-01-05 11:46:37 +0000

    Create sum_double.py

---
+++ Warmup-1/sum_double.py
@@ -0,0 +1,9 @@
+# if a is equal to b, return double the sum of a and b
+# otherwise return the sum of a and b
+
+def sum_double(a, b):
+  sum = a + b
+  
+  if a == b :
+    return 2 * sum
+  return sum
