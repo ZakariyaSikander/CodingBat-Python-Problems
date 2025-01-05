440b50e33a70c1d00dbfc39e7034f6d43bc9dc05
ZakariyaSikander <zakariya.sikander@gmail.com>
2025-01-05 11:50:46 +0000

    Create diff21.py

---
+++ Warmup-1/diff21.py
@@ -0,0 +1,8 @@
+# if n is greater than 21, return the absolute difference between n and 21
+# otherwise return the absolute difference between n and 21
+
+def diff21(n):
+  
+  if n > 21 :
+    return 2 * (n - 21)
+  return (21 - n)
