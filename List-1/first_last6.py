9a639b6c7daeb43b98006531f7c8b24046f0902f
ZakariyaSikander <zakariya.sikander@gmail.com>
2025-01-05 12:40:21 +0000

    Create first_last6.py

---
+++ List-1/first_last6.py
@@ -0,0 +1,4 @@
+# Given an array of ints, return True if 6 appears as either the first or last element in the array.
+
+def first_last6(nums):
+  return True if nums[0] == 6 or nums[-1] == 6 else False
